64f80cac96e83527b2bab62295e606275f3c75aa
sreardonyext <100706230+sreardonyext@users.noreply.github.com>
2023-04-25 10:21:18 -0400

    Add files via upload

---
+++ ecl-menu-connector/platform/plugin/pullingDescriptions/mod.ts
@@ -0,0 +1,53 @@
+export async function fetchInformation(menuId) {
+
+    var requestUrl = "https://api.yextapis.com/v2/accounts/me/menus/" + menuId + "?v=20230324&api_key=${{apiKey}}" // the menu endpoint. make sure to switch the API key to a variable when you publish
+
+    const response = await fetch(requestUrl).then(response => response.json()) // this is how you make an API call with a function. don't worry too much about the notation
+
+    var myOutputArray = []
+
+    for (var x = 0; x < response.response.sections.length; x++) {
+
+        var myIds = []
+        var myNames = []
+        var myDescriptions = []
+        var myCosts = []
+        var myImages = []
+
+        for (var y = 0; y < response.response.sections[x].items.length; y++) {
+
+            myIds.push(response.response.sections[x].items[y].id)
+            myNames.push(response.response.sections[x].items[y].name)
+            // myCosts.push(response.response.sections[x].items[y].cost.price)
+            //myImages.push(response.response.sections[x].items[y].photo.url)
+            
+            if(typeof response.response.sections[x].items[y].cost.price == 'undefined') {
+                myCosts.push("")
+            }
+            else {
+                myCosts.push(response.response.sections[x].items[y].cost.price)
+            }
+
+            if(typeof response.response.sections[x].items[y].photo == 'undefined') {
+                myImages.push("")
+            }
+            else {
+                myImages.push(response.response.sections[x].items[y].photo.url)
+            }
+
+            if(typeof response.response.sections[x].items[y].description == 'undefined') {
+                myDescriptions.push("")
+            }
+            else {
+                myDescriptions.push(response.response.sections[x].items[y].description)
+            }
+
+        }
+
+        myOutputArray.push(myIds,"***",myNames,"***",myDescriptions,"***",myCosts,"***",myImages,"***")
+
+    }
+
+    return JSON.stringify(myOutputArray)
+}
+
